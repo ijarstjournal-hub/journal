ea8d18896ece03bf9ba76cdd5562921ca2156dfc
ijarstjournal-hub <ijarstjournal@gmail.com>
2026-03-18 02:22:44 +0100

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,7 @@ import axios from 'axios';
 import './index.css';
 import App from './App';
 
-// configure axios for both development and production
-// REACT_APP_API_URL should be set in your hosting environment (Vercel/Render)
-axios.defaults.baseURL = process.env.REACT_APP_API_URL || '';
-axios.defaults.withCredentials = true;
+axios.defaults.baseURL = '';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
